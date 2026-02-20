a1aee7d2a0209cad93fb036f1cfdedffa8c7992a
LucasVieira0620 <133678724+LucasVieira0620@users.noreply.github.com>
2026-02-20 14:11:59 -0300

    Add files via upload

---
+++ cdproducts.py
@@ -0,0 +1,103 @@
+""" 
+Este sistema tem como objetivo simular um controle básico de produtos,
+sem uso de banco de dados, utilizando apenas Python e estruturas nativas da linguagem
+
+O sistema foi pensado para funcionar em terminal (console) e será
+organizado em funções, facilitando a leitura, manutenção e evolução
+do código.
+
+O sistema conta com login, cadastro de produtos com valores e estoque, listagem de produtos, atualizar estoque ou valores.
+"""
+
+banco = {} #Local que armazena os produtos, valores e estoque
+menu = -1 #variável que permite loop do menu
+
+def sucesso(msg): #Função para mensagem de sucesso
+    print(f"\n\033[32m{msg}\033[m")
+
+def erro(msg): #Função para mensagem de erro
+    print(f"\n\033[31m{msg}\033[m")
+
+def pausa(): #Pausa o script para ler as informações, evitando que o menu aparece toda hora.
+    input("\nPrecione Enter para voltar ao menu...")
+    
+
+
+def cadastro(): #Função de cadastro de produtos
+    produto = input("Qual produto quer cadastrar?: ")
+    if produto in banco: #Verifica se o produto já existe no sistema, evita duplicidade
+        erro("Produto já cadastrado no sistema")
+        return
+    valor = float(input("Qual o valor do produto?: "))
+    estoque = int(input(f"Quantas unidades de {produto} tem no estoque?: "))
+    banco[produto] = {"valor": valor, "estoque": estoque} 
+    sucesso("Produto cadastrado com sucesso")
+    #Aqui ele cria um dicionário dentro do dicionário. Banco é como a gaveta, produto é uma pochete dentro da gaveta e "valor" e "estoque são informações dentro da pochete. Dentro de um dicionário os itens são dividos como chaves e valores, o que está entre "" são as chaves, o que está fora, são valores.
+
+def listar(): #Função que lista todos os produtos cadastrados
+    print("Aqui estão todos os produtos cadastrados: ")
+    for produto in banco:
+        valor = banco[produto]["valor"] #Identifica o que é valor no dicionário para puxar no print.
+        estoque = banco[produto]["estoque"] #Identifica o que é estoque no dicionário para puxar no print 
+        print(f"\nProduto: {produto} | Preço: {valor} | Estoque: {estoque}")
+    pausa()
+def atualizar(): #Função para atualizar as informações do produto
+    produto = input("Qual produto você deseja editar?: ")
+    if produto not in banco:
+        erro("Produto não encontrado no sistema")
+        return
+    print("=== MENU ===")
+    print("1 - Atualizar nome ")
+    print("2 - Atualizar valor ")
+    print("3 - Atualizar estoque ")
+    mini_menu = int(input("Digite a opção desejada: "))
+    if mini_menu == 1:
+        novo_nome = input("Digite o novo nome do produto: ")
+        if novo_nome in banco: #Verifica se o novo nome do produto já existe no sistema
+            erro("Esse nome de produto já está cadastrado no sistema")
+            return
+        banco[novo_nome] = banco.pop(produto) #atualiza o nome do produto no banco de dados, tirando o nome antigo e colocando o novo sem mexer nas chaves
+        sucesso("\nNovo nome de produto atualizado com sucesso!")
+        pausa()
+    
+    elif mini_menu == 2: #Menu para atualizar valor do produto
+        novo_valor = float(input("Digite o novo valor do produto: "))
+        banco[produto]["valor"] = novo_valor
+        sucesso("Valor do produto atualizado com sucesso")
+        pausa()
+    
+    elif mini_menu == 3: #Menu para atualizar estoque do produto
+        novo_estoque = int(input("Digite o novo valor do estoque: "))
+        banco[produto]["estoque"] = novo_estoque
+        sucesso("Estoque do produto atualizado com sucesso")
+        pausa()
+    
+
+while menu != 0: #Loop para manter o menu funcionando
+    print("-"*70)
+    print("========== MENU ==========")
+    print("Escolha uma das seguintes opções:")
+    print("1 - Cadastrar um novo produto")
+    print("2 - Atualizar preço ou estoque do produto cadastrado")
+    print("3 - Listar todos os produtos cadastrados")
+    menu = int(input("Digite aqui a opção desejada: "))
+
+    if menu == 1:
+        cadastro()
+    
+    elif menu == 2:
+        atualizar()
+
+    elif menu == 3:
+        listar()
+    
+    elif menu not in (0,1,2,3):
+        erro("Digite uma opção válida")
+
+erro("Programa encerrado") #Caso o usuário digite 0
+            
+
+
+        
+
+
